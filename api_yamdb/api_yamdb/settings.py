3aa36f07c8a811cd7f1d615ca14d032efd190cbd
Anton Ign <i_a_v_86@mail.ru>
2023-04-26 14:04:04 +0300

    fxs

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,6 +1,7 @@
 import os
-from datetime import timedelta
+
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
