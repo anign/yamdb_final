a74c8219b6dff35194909ef80c40677b5515f367
Anton Ign <i_a_v_86@mail.ru>
2023-05-01 22:31:09 +0300

    final fixes god bless me pls v2.0

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Title, Review, Comment, Genre
+from .models import Comment, Genre, Review, Title
 
 admin.site.register(Title)
 admin.site.register(Review)
